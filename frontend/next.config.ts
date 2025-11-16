4b87416fe04e70a8d10f1b25f3a15fb2f9fd76d2
TFT <artoreinik@gmail.com>
2025-11-16 18:28:03 +0200

    containerize frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
     typescript: {
         ignoreBuildErrors: true,
-    }
+    },
+    output: 'standalone',
 };
 
 export default nextConfig;
